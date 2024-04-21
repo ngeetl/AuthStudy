b4bf0a800548d678c45b224cb8f896b1e9ef629c
yeni <ngeetl@naver.com>
2024-04-21 13:32:13 +0900

    Feat/Design: SignInPage UI 생성 및 프론트 로직 구현

--- frontend/src/pages/SignUpPage.tsx
+++ frontend/src/pages/SignUpPage.tsx
@@ -33,7 +33,6 @@ const SignUpPage: React.FC = () => {
     };
 
 
-
     return (
         <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
             <form onSubmit={handleSubmit(onSubmit)} className="p-8 bg-white rounded shadow-md w-1/3">
